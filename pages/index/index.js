7adc6023558e0a042eef90546e3a87fd6e894437
Zhao-666 <2268699370@qq.com>
2017-12-12 14:30:02 +0800

    V0.0.7

---
+++ pages/index/index.js
@@ -0,0 +1,203 @@
+var Charts = require('../../utils/wxcharts.js')
+
+Page({
+  data: {
+    chartIndex: 0,
+    chartArray: [{
+      value: 1,
+      name: '终端DLP事件风险趋势'
+    }, {
+      value: 5,
+      name: '网络DLP事件风险趋势'
+    }, {
+      value: 2,
+      name: '安全基线违规事件风险趋势'
+    }, {
+      value: 3,
+      name: '非法外联违规事件风险趋势'
+    }, {
+      value: 4,
+      name: '防病毒扫描事件趋势'
+    }],
+    chartData: {},
+    chartWidth: 0,
+    chartHeight: 0,
+    newEventsList: [],
+    chartReady: false,
+    eventReady: false,
+    timeType: 1, //1-一个月 2-半年 3-一年
+    timeTitle: ['', '30 天', '半 年', '一 年'],
+    ballOpacity: 0.7,
+    ballBottom: 20,
+    ballRight: 30
+  },
+  loadChart: function () {
+    console.log("loadChart:")
+    let result = this.formatChartData()
+    var chartWidth = this.data.chartWidth
+    var chartHeight = this.data.chartHeight
+    new Charts({
+      canvasId: 'chart-canvas',
+      background: '#2d3235',
+      type: 'area',
+      categories: result.date,//横坐标
+      animation: true,
+      dataLabel: false,
+      series: [{
+        name: '日期',
+        data: result.count, //纵坐标
+        color: '#448ff9'
+      }],
+      yAxis: {
+        min: 0,
+        fontColor: '#dddddd',
+        gridColor: '#dddddd',
+        format: function (val) {
+          return val
+        },
+      },
+      xAxis: {
+        fontColor: '#dddddd',
+        gridColor: '#dddddd'
+      },
+      extra: {
+        legendTextColor: '#dddddd',
+        lineStyle: 'curve'
+      },
+      width: chartWidth,
+      height: chartHeight
+    })
+    this.setData({
+      chartReady: true
+    })
+  },
+  onLoad: function () {
+    var res = wx.getSystemInfoSync()
+    let chartWidth = res.windowWidth - 20
+    let chartHeight = res.windowHeight / 3
+    let screenWidth = res.windowWidth
+    let screenHeight = res.windowHeight
+    this.setData({
+      chartWidth,
+      chartHeight,
+      screenWidth,
+      screenHeight
+    })
+    this.getChartData()
+    this.getNewEvents()
+  },
+  getChartData() {
+    console.log('getChartData')
+    let that = this
+    let chartID = this.data.chartArray[this.data.chartIndex].value
+    let timeType = this.data.timeType
+    wx.request({
+      url: 'http://10.200.16.19:8080/EventsRiskTrend/?type=' + timeType + '&id=' + chartID + '&XDEBUG_SESSION_START=17413',
+      method: 'GET',
+      success: function (res) {
+        let chartData = res.data.data
+        console.log(chartData)
+        that.setData({
+          chartData
+        })
+        that.loadChart()
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  getNewEvents() {
+    console.log('getNewEvent')
+    let that = this
+    wx.request({
+      url: 'http://10.200.16.19:8080/EventsInfoNews?XDEBUG_SESSION_START=10334',
+      method: 'GET',
+      success: function (res) {
+        let newEventsList = res.data.data
+        that.setData({
+          newEventsList,
+          eventReady: true
+        })
+        console.log(newEventsList)
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  formatChartData() {
+    console.log('formatChartData:')
+    let result = {
+      date: [],
+      count: []
+    }
+    let chartData = this.data.chartData
+    for (let i = 0; i < chartData['sequence'].length; i++) {
+      result.date[i] = chartData['sequence'][i].date
+      result.count[i] = chartData['sequence'][i].count
+    }
+    if (result.date[0] == undefined) {
+      result = {
+        date: [1, 2, 3, 4, 5],
+        count: [0, 0, 0, 0, 0]
+      }
+    }
+    console.log(result)
+    return result
+  },
+  refreshEvent() {
+    this.setData({
+      newEventsList: [],
+      eventReady: false
+    })
+    this.getNewEvents()
+  },
+  changeChart(e) {
+    let chartIndex = e.detail.value;
+    this.setData({
+      chartIndex,
+      chartData: {},
+      chartReady: false
+    })
+    this.getChartData()
+  },
+  changeTimeType() {
+    let timeType = this.data.timeType + 1
+    timeType = timeType > 3 ? 1 : timeType
+    this.setData({
+      timeType,
+      chartData: {},
+      chartReady: false
+    })
+    this.getChartData()
+  },
+  ballClickStart() {
+    this.setData({
+      ballOpacity: 1
+    })
+  },
+  ballClickEnd() {
+    this.setData({
+      ballOpacity: 0.7
+    })
+  },
+  ballClickEvent() {
+
+  },
+  ballMoveEvent(e) {
+    var touchs = e.touches[0];
+    var pageX = touchs.pageX;
+    var pageY = touchs.pageY;
+    if (pageX < 25) return;
+    if (pageX > this.data.screenWidth - 25) return;
+    if (this.data.screenHeight - pageY <= 25) return;
+    if (pageY <= 25) return;
+    var x = this.data.screenWidth - pageX - 25;
+    var y = this.data.screenHeight - pageY - 25;
+    this.setData({
+      ballBottom: y,
+      ballRight: x
+    });
+  }
+})
---
+++ pages/login/login.js
@@ -0,0 +1,80 @@
+// pages/login/login.js
+Page({
+  data: {
+    passWord: '',
+    userNameTips: '',
+    passWordTips: ''
+  },
+  formSubmit: function (e) {
+    console.log(e)
+    let username = e.detail.value.username
+    let password = e.detail.value.password
+    this.setData({
+      userNameTips: '',
+      passWordTips: ''
+    })
+
+    let tips = this.loginValidate(username, password)
+    console.log(tips.userNameTips)
+    this.setData({
+      userNameTips: tips.userNameTips,
+      passWordTips: tips.passWordTips
+    })
+    if (tips.userNameTips === undefined && tips.passWordTips === undefined) {
+      let that = this
+      wx.request({
+        url: 'http://localhost:8080/Login',
+        data: {
+          username: username,
+          password: password,
+        },
+        method: 'POST',
+        success: function (res) {
+          console.log(res)
+          if (res.data.errorCode === '10001') {
+            that.setData({
+              passWordTips: res.data.msg
+            })
+          } else {
+            wx.navigateTo({
+              url: '../index/index',
+            })
+          }
+        },
+        fail: function (res) {
+          console.log(res)
+          console.log(res.data.msg)
+          that.setData({
+            passWordTips: res.data.msg
+          })
+        }
+      })
+    }
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
+    this.setData({
+      passWordTips: ''
+    })
+  },
+  loginValidate: function (username, password) {
+    let tips = {}
+    if (username === '') {
+      tips.userNameTips = '请输入账户'
+    } else if (username.length < 6) {
+      tips.userNameTips = '账户字符须大于六位'
+    }
+    if (password === '') {
+      tips.passWordTips = '请输入密码'
+    } else if (password.length < 8) {
+      tips.passWordTips = '密码字符须大于八位'
+    }
+    return tips
+  },
+  cleanPassword: function () {
+    console.log('清空密码哦')
+    this.setData({
+      passWord: ''
+    })
+  }
+})
